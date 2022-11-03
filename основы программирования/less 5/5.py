1f1bddb4552962cc7cff3ec82bd950e6fdc520ad
nocvvi <dima-vrn00@inbox.ru>
2022-11-03 16:31:35 +0300

    changed 5 and 6

--- основы программирования/less 5/5.py
+++ основы программирования/less 5/5.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-x = 0
-while int(input('add number:')) != 0:
-    x += 1
-print(x)
+def num():
+    x = 0
+    while int(input('add number:')) != 0:
+        x += 1
+    print(x)
+num()
--- основы программирования/less 5/6.py
+++ основы программирования/less 5/6.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
-n = 1
-s = 0
-while n != 0:
-    n = int(input('add n:'))
-    s += n
-print(s)
+def n():
+    n = 1
+    s = 0
+    while n != 0:
+        n = int(input('add n:'))
+        s += n
+    print(s)
+n()
