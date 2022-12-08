78867e002db1bf38346b0cdc9c5b6ac36df6941d
nocvvi <dima-vrn00@inbox.ru>
2022-12-08 22:09:12 +0300

    update 12.2b

--- основы программирования/less 12/2b.py
+++ основы программирования/less 12/2b.py
@@ -1,20 +1,9 @@
 # -*- coding: utf-8 -*-
-
-a = int(input('enter num='))
-def posled(a):
-    s = []
-    s.append(a)
-    while str(a) != '0':
-        b = int(input('enter num='))
-        if str(b) != '0':
-            s.append(a)
-            s.append(b)
-            mins = min(s)
-            maxim = max(s)
-            for i in range(len(s)):
-                if s[i] > mins and s[i] < maxim:
-                    mins = s[i]
-        else:
-            print(mins)
-            break
-print(posled(a))
+def posled():
+    n = int(input('add number:'))
+    if n == 0:
+        return [float('-inf')]
+    return sorted([n] + posled())
+def run():
+    print(posled()[-2])
+run()
